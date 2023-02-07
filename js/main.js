3db239bbbb04c8c6f60feeb0dc9de5c75326bc38
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-02-07 18:24:31 +0100

    done

---
+++ js/main.js
@@ -0,0 +1,109 @@
+// Consegna:
+// Dato un array di oggetti letterali con:
+//  - url dell'immagine
+//  - titolo
+//  - descrizione
+// Creare un carosello come nella foto allegata.
+// Milestone 0:
+// Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
+// Milestone 1:
+// Ora rimuoviamo i contenuti statici e usiamo l'array di oggetti letterali per popolare dinamicamente il carosello.
+// Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+// Milestone 2:
+// Aggiungere il **ciclo infinito** del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
+// BONUS 1:
+// Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l'immagine corrispondente.
+// BONUS 2:
+// Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l'immagine attiva dovrà cambiare alla successiva.
+// BONUS 3:
+// Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
+
+/********************************
+            ON LOAD             *
+*********************************/
+
+const buttonNextEl = document.querySelector(".next");
+const buttonPrevEl = document.querySelector(".prev");
+
+const images = [
+  {
+    image: "img/01.webp",
+    title: "Marvel's Spiderman Miles Morale",
+    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+  },
+  {
+    image: "img/02.webp",
+    title: "Ratchet & Clank: Rift Apart",
+    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+  },
+  {
+    image: "img/03.webp",
+    title: "Fortnite",
+    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+  },
+  {
+    image: "img/04.webp",
+    title: "Stray",
+    text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+  },
+  {
+    image: "img/05.webp",
+    title: "Marvel's Avengers",
+    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+  },
+];
+// console.log(images);
+// console.log(images[1]);
+// console.log(images[1].image);
+
+const sliderEL = document.getElementById("slider");
+for (let i = 0; i < images.length; i++) {
+  sliderEL.innerHTML += `    
+  <figure class="item text-center">
+    <img class= "image" src="${images[i].image}" alt="poto"></img>
+    <figcaption>
+      <h2 class="title">${images[i].title}</h2>
+      <h3 class="text">${images[i].text}</h3>
+    </figcaption>
+  </figure> 
+ `;
+}
+console.log(sliderEL);
+
+const allItems = document.querySelectorAll(".item");
+allItems[0].classList.add("active");
+
+/********************************
+      ON CLICK DEL TASTO PREV   *
+*********************************/
+let slideCorrente = 0;
+
+buttonPrevEl.addEventListener("click", function () {
+  // const allItems = document.querySelectorAll(".item");
+  allItems[slideCorrente].classList.remove("active");
+
+  slideCorrente--;
+
+  if (slideCorrente < 0) {
+    slideCorrente = allItems.length - 1;
+  }
+  console.log(slideCorrente);
+
+  allItems[slideCorrente].classList.add("active");
+});
+
+/********************************
+    ON CLICK DEL TASTO NEXT     *
+*********************************/
+
+buttonNextEl.addEventListener("click", function () {
+  // const allItems = document.querySelectorAll(".item");
+  allItems[slideCorrente].classList.remove("active");
+
+  slideCorrente++;
+
+  if (slideCorrente >= allItems.length) {
+    slideCorrente = 0;
+  }
+  allItems[slideCorrente].classList.add("active");
+});
